05b8b6b10b7696f3e8b8db7a241c2ec02755e01a
RanaKhamis <ranakhms@gmail.com>
2022-01-30 23:34:02 +0200

    Solved a simple issue for multiple messages for 1 chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,9 +4,9 @@ class Message < ApplicationRecord
 
   belongs_to :chat, counter_cache: :message_count, touch: true
 
-  validates :chat_id,
+  validates :message_id,
           presence: true,
-          numericality: { only_integer: true, greater_than_or_equal_to: 1 },
+          #numericality: { only_integer: true, greater_than_or_equal_to: 1 },
           uniqueness: { scope: :chat_id }
 
   include Searchable
